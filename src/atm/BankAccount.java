3b70105c76ec6aff3095350b0aa0e81519042ccd
tui r <ratanachai.sr@rd.go.th>
2020-08-24 15:30:35 +0700

    initial commit

---
+++ src/atm/BankAccount.java
@@ -0,0 +1,65 @@
+package atm;
+
+public class BankAccount {
+    private int id;
+    private String name;
+    private String type;
+    private double balance;
+
+    public BankAccount(int id, String name, String type) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.balance = 0;
+    }
+
+    public BankAccount(int id, String name, String type, double balance) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.balance = balance;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void withDraw(double amount) {
+        this.balance -= amount;
+    }
+
+    public void deposit(double amount) {
+        this.balance += amount;
+    }
+
+    public void transfer(BankAccount to, double amount){
+        this.withDraw(amount);
+        to.deposit(amount);
+    }
+
+    public void validateCustomer(){
+
+    }
+
+    @Override
+    public String toString() {
+        return "BankAccount{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", type='" + type + '\'' +
+                ", balance=" + balance +
+                '}';
+    }
+}
